b1cd2c8b68ff800b59ecc07f662d6f6d37101158
Luke <68408655+lukeoxner@users.noreply.github.com>
2021-05-06 21:14:38 -0600

    initialize Our Picks page

---
+++ client/src/pages/OurPicks.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import { Grid, Container } from "@material-ui/core/";
+import Details from "../components/Details";
+
+function Home() {
+	// create state - useState
+	const [results, setResults] = useState([]);
+
+	// make a call to API to get some data - useEffect
+	useEffect(() => {
+		// make API call here
+		fetch(
+			"https://api.openbrewerydb.org/breweries?by_state=colorado&per_page=50"
+		)
+			.then((res) => res.json())
+			.then((data) => setResults(data));
+	}, []);
+
+	return (
+		<>
+			<h1>Search Page</h1>
+			<Container>
+				<Grid container direction="row" justify="center" alignItems="center">
+					{results.map((result) => (
+						<Grid item key={result.id}>
+							<Details
+								id={result.id}
+								key={result.id}
+								name={result.name}
+								street={result.street}
+								city={result.city}
+								state={result.state}
+								website={result.website_url}
+								type={result.brewery_type}
+							/>
+						</Grid>
+					))}
+				</Grid>
+			</Container>
+		</>
+	);
+}
+
+export default Home;
