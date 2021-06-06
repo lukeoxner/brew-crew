677015959bbb0da744da79352184a8bca3ce9ca3
Luke <68408655+lukeoxner@users.noreply.github.com>
2021-06-06 17:59:56 -0500

    search route allllmost working

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -9,16 +9,6 @@ const useStyles = makeStyles((theme) => ({
 		display: "flex",
 		flexWrap: "wrap",
 	},
-	// textField: {
-	// 	marginLeft: theme.spacing(1),
-	// 	marginRight: theme.spacing(1),
-	// 	width: "25ch",
-	// },
-	// container: {
-	// 	display: "grid",
-	// 	gridTemplateColumns: "repeat(12, 1fr)",
-	// 	gridGap: theme.spacing(3),
-	// },
 }));
 
 function Search() {
@@ -33,61 +23,85 @@ function Search() {
 
 	// make a call to API to get some data - useEffect
 	useEffect(() => {
-		// make API call here
-		fetch(
-			"https://api.openbrewerydb.org/breweries?by_state=colorado&per_page=50"
-		)
-			.then((res) => res.json())
-			.then((data) => setResults(data));
+		setResults(API.search());
 	}, []);
 
 	const onChange = (e) => {
 		console.log(e.target.value);
 		setResults(API.search(e.target.value));
+		// console.log(API.search(e.target.value));
+		console.log(results);
 	};
 
-	return (
-		<>
-			<h1>Search Page</h1>
-			<Container>
-				<Grid container direction="row" justify="center" alignItems="center">
-					<Grid item sm={11} lg={8}>
-						<div className={classes.root}>
-							<TextField
-								id="search"
-								style={{ margin: 8 }}
-								placeholder="Search"
-								fullWidth
-								margin="normal"
-								InputLabelProps={{
-									shrink: true,
-								}}
-								onChange={onChange}
-							/>
-						</div>
+	if (!results) {
+		return (
+			<>
+				<h1>Search Page</h1>
+				<Container>
+					<Grid container direction="row" justify="center" alignItems="center">
+						<Grid item sm={11} lg={8}>
+							<div className={classes.root}>
+								<TextField
+									id="search"
+									style={{ margin: 8 }}
+									placeholder="Search"
+									fullWidth
+									margin="normal"
+									InputLabelProps={{
+										shrink: true,
+									}}
+									onChange={onChange}
+								/>
+							</div>
+						</Grid>
 					</Grid>
-				</Grid>
-				<Grid container direction="row" justify="center" alignItems="center">
-					{results
-						? results.map((result) => (
-								<Grid item key={result.id}>
-									<Details
-										id={result.id}
-										key={result.id}
-										name={result.name}
-										street={result.street}
-										city={result.city}
-										state={result.state}
-										website={result.website_url}
-										type={result.brewery_type}
-									/>
-								</Grid>
-						  ))
-						: console.log("yo")}
-				</Grid>
-			</Container>
-		</>
-	);
+				</Container>
+			</>
+		);
+	} else {
+		return (
+			<>
+				<h1>Search Page</h1>
+				<Container>
+					<Grid container direction="row" justify="center" alignItems="center">
+						<Grid item sm={11} lg={8}>
+							<div className={classes.root}>
+								<TextField
+									id="search"
+									style={{ margin: 8 }}
+									placeholder="Search"
+									fullWidth
+									margin="normal"
+									InputLabelProps={{
+										shrink: true,
+									}}
+									onChange={onChange}
+								/>
+							</div>
+						</Grid>
+					</Grid>
+					<Grid container direction="row" justify="center" alignItems="center">
+						{results
+							? results.map((result) => (
+									<Grid item key={result.id}>
+										<Details
+											id={result.id}
+											key={result.id}
+											name={result.name}
+											street={result.street}
+											city={result.city}
+											state={result.state}
+											website={result.website_url}
+											type={result.brewery_type}
+										/>
+									</Grid>
+							  ))
+							: console.log("yo")}
+					</Grid>
+				</Container>
+			</>
+		);
+	}
 }
 
 export default Search;
--- client/src/utils/BreweryAPI.js
+++ client/src/utils/BreweryAPI.js
@@ -5,15 +5,14 @@ import axios from "axios";
 export default {
 	search: function (query) {
 		fetch(
-			`https://api.openbrewerydb.org/breweries/search?${query}=&per_page=25`
+			`https://api.openbrewerydb.org/breweries/search?query=${query}&per_page=25`
 		)
 			.then((res) => res.json())
 			.then(function (data) {
 				let theResult = data;
-				console.log(theResult);
+				// console.log(theResult);
 				return theResult;
 			});
-		// .then((data) => setResults(data));
 	},
 	getAll: function () {
 		return axios.get("https://api.openbrewerydb.org/breweries");
