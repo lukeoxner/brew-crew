6f08c9d3760442a8ffa55913e492b8ce4c670d3b
Luke <68408655+lukeoxner@users.noreply.github.com>
2021-06-06 15:28:30 -0500

    adding search bar

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -21,10 +21,31 @@ function Search() {
 			.then((data) => setResults(data));
 	}, []);
 
+	const onChange = (e) => {
+		setResults(API.search(e.target.value));
+	};
+
 	return (
 		<>
 			<h1>Search Page</h1>
 			<Container>
+				<Grid container direction="row" justify="center" alignItems="center">
+					<Grid item sm={11} lg={8}>
+						<div className={classes.root}>
+							<TextField
+								id="search"
+								style={{ margin: 8 }}
+								placeholder="Search"
+								fullWidth
+								margin="normal"
+								InputLabelProps={{
+									shrink: true,
+								}}
+								onChange={onChange}
+							/>
+						</div>
+					</Grid>
+				</Grid>
 				<Grid container direction="row" justify="center" alignItems="center">
 					{results.map((result) => (
 						<Grid item key={result.id}>
