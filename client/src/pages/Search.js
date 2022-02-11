9eae94dec5df23a5d9e5e0226195de20f0cd3336
Luke <68408655+lukeoxner@users.noreply.github.com>
2022-02-11 13:57:09 -0700

    styling for search bar and page

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -10,22 +10,31 @@ const useStyles = makeStyles((theme) => ({
 		'& .MuiInputBase-root': {
 			background: '#1f1f1f',
 			color: '#fff',
-			fontSize: '20px',
+			// fontSize: '20px',
 			padding: '10px',
 			paddingLeft: '25px',
-			margin: '15px',
+			marginTop: '15px',
+			marginBottom: '15px',
 			display: 'block',
-			width: '400px',
+			width: '100%',
 			textAlign: 'center',
+			outlineColor: '#f1a922',
+			outlineStyle: 'solid',
+			outlineWidth: '2px',
 			borderRadius: '15px',
 		},
 		display: 'flex',
 		flexWrap: 'wrap',
-		justifyContent: 'center',
 	},
 	background: {
 		backgroundColor: '#606060',
 		minHeight: '100vh',
+	},
+	header: {
+		textAlign: 'center',
+	},
+	searchGrid: {
+		paddingTop: '100px',
 	},
 }));
 
@@ -38,103 +47,67 @@ function Search() {
 
 	// make a call to API to get some data - useEffect
 	useEffect(() => {
-		console.log('useEffect called');
-		console.log(searchTerm);
 		fetch(
 			`https://api.openbrewerydb.org/breweries/search?query=${searchTerm}&per_page=25`
 		)
 			.then((res) => res.json())
 			.then(function (data) {
 				setResults(data);
-				console.log(data);
 			});
 	}, [searchTerm]);
 
 	const onChange = (e) => {
-		console.log(`e.target.value: ${e.target.value}`);
+		// console.log(`e.target.value: ${e.target.value}`);
 		let searchInput = e.target.value;
 		let formattedSearchInput = searchInput.replace(' ', '_');
 		setSearchTerm(formattedSearchInput);
 	};
 
-	if (!results) {
-		return (
-			<>
-				<div className={classes.background}>
-					<h1>Search Page</h1>
-					<Container>
-						<Grid
-							container
-							direction='row'
-							justify='center'
-							alignItems='center'
-						>
-							<Grid item sm={11} lg={8}>
-								<div className={classes.root}>
-									<InputBase
-										onChange={onChange}
-										placeholder='Search breweries…NO RESULTS...'
-										inputProps={{ 'aria-label': 'search' }}
-									/>
-								</div>
-							</Grid>
+	return (
+		<>
+			<div className={classes.background}>
+				<Container>
+					<Grid
+						container
+						direction='row'
+						justify='center'
+						alignItems='center'
+						className={classes.searchGrid}
+					>
+						<Grid item xs={10} sm={6} lg={4}>
+							<div className={classes.root}>
+								<InputBase
+									onChange={onChange}
+									placeholder='Search breweries…'
+									inputProps={{
+										'aria-label': 'search',
+									}}
+								/>
+							</div>
 						</Grid>
-					</Container>
-				</div>
-			</>
-		);
-	} else {
-		return (
-			<>
-				<div className={classes.background}>
-					<h1>Search Page</h1>
-					<Container>
-						<Grid
-							container
-							direction='row'
-							justify='center'
-							alignItems='center'
-						>
-							<Grid item sm={11} lg={8}>
-								<div className={classes.root}>
-									<InputBase
-										onChange={onChange}
-										placeholder='Search breweries…'
-										inputProps={{
-											'aria-label': 'search',
-										}}
-									/>
-								</div>
-							</Grid>
-						</Grid>
-						<Grid
-							container
-							direction='row'
-							justify='center'
-							alignItems='center'
-						>
-							{results
-								? results.map((result) => (
-										<Grid item key={result.id}>
-											<Details
-												id={result.id}
-												key={result.id}
-												name={result.name}
-												street={result.street}
-												city={result.city}
-												state={result.state}
-												website={result.website_url}
-												type={result.brewery_type}
-											/>
-										</Grid>
-								  ))
-								: console.log('yo')}
-						</Grid>
-					</Container>
-				</div>
-			</>
-		);
-	}
+					</Grid>
+					<Grid container direction='row' justify='center' alignItems='center'>
+						{results
+							? results.map((result) => (
+									<Grid item key={result.id}>
+										<Details
+											id={result.id}
+											key={result.id}
+											name={result.name}
+											street={result.street}
+											city={result.city}
+											state={result.state}
+											website={result.website_url}
+											type={result.brewery_type}
+										/>
+									</Grid>
+							  ))
+							: console.log('yo')}
+					</Grid>
+				</Container>
+			</div>
+		</>
+	);
 }
 
 export default Search;
